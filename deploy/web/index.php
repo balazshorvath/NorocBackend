8129661aa9f38dae6f05e7f1acfeaf8e7f5ee0c1
balazshorvath <balazs.peter.horvath@gmail.com>
2016-06-08 14:32:15 +0200

    php fix

--- deploy/web/index.php
+++ deploy/web/index.php
@@ -10,7 +10,7 @@
             $error = "The username must be at least 4 characters long (maximum 10) and can not contain special characters.";
         }
         if(strlen($_POST["password"]) <= 4
-            && preg_match("^.{4,32}", $_POST["username"])){
+            && preg_match("^.{4,32}$", $_POST["username"])){
             $error = "The password must contain at least 4 characters (maximum 32).";
         }
         if(strcmp($_POST["passwordConfirm"], $_POST["password"])){
